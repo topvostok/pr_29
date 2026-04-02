ee8e7aabb8f0f3633953dab77d0c07e01c550482
Linashihova <linashikhova@mail.ru.>
2026-04-02 09:30:05 +0500

    Разработанные контексты для моделей Peshin

---
+++ Classes/ClubsContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PG_29.Common;
+using PG_29.Models;
+
+namespace WpfApp1.Classes
+{
+    public class ClubsContext : DbContext  
+    {
+        public DbSet<Clubs> Clubs { get; set; }
+
+        public ClubsContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql(Config.ConnectionConfig, Config.Version);
+            }
+        }
+        public void EnsureDatabaseCreated()
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
--- Common/Config.cs
+++ Common/Config.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace PG_29.Common
 {
     class Config
     {
+        public static string ConnectionConfig = "";
+        public static MySqlServerVersion Version = new MySqlServerVersion(new Version(8, 0, 11));
 
     }
 }
